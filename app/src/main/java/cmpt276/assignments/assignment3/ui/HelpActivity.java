93cb1b32a80c9ba25cb104675000323dae35a353
Jerry <zrd1@sfu.ca>
2021-10-14 20:10:27 -0700

    Add navigation buttons to the main menu.
    Implemented intents from welcome screen to the menu, and from menu to the help screen.
    
    Added functionality to close the app from Main menu by clicking the Android back button.
    
    Added content to welcome screen and the help screen.

--- app/src/main/java/cmpt276/assignments/assignment3/ui/HelpActivity.java
+++ app/src/main/java/cmpt276/assignments/assignment3/ui/HelpActivity.java
@@ -2,6 +2,8 @@ package cmpt276.assignments.assignment3.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import cmpt276.assignments.assignment3.R;
@@ -13,4 +15,12 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
     }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, HelpActivity.class);
+    }
+
+
+
+
 }
--- app/src/main/java/cmpt276/assignments/assignment3/ui/MainMenuActivity.java
+++ app/src/main/java/cmpt276/assignments/assignment3/ui/MainMenuActivity.java
@@ -2,8 +2,13 @@ package cmpt276.assignments.assignment3.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import cmpt276.assignments.assignment3.MainActivity;
 import cmpt276.assignments.assignment3.R;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -12,5 +17,36 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setHelpButton();
+
+
     }
+
+    // https://stackoverflow.com/questions/18404271/android-back-button-to-specific-activity
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        // https://developer.android.com/reference/android/app/Activity
+        super.moveTaskToBack(true);
+    }
+
+    private void setHelpButton() {
+        Button helpBtn = findViewById(R.id.helpBtn);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // make intent to launch HelpActivity
+                Intent helpIntent = HelpActivity.makeIntent(MainMenuActivity.this);
+                startActivity(helpIntent);
+            }
+        });
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, MainMenuActivity.class);
+    }
+
+
 }
