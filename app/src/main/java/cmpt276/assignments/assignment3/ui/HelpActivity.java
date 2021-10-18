131296c4aabf540215c22c9351cba13687bc9ce0
Jerry <zrd1@sfu.ca>
2021-10-17 17:19:27 -0700

    Implement activity_help.xml text details and reformat text in activity_main.xml

--- app/src/main/java/cmpt276/assignments/assignment3/ui/HelpActivity.java
+++ app/src/main/java/cmpt276/assignments/assignment3/ui/HelpActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import cmpt276.assignments.assignment3.R;
 
@@ -14,13 +16,13 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        // https://stackoverflow.com/questions/1748977/making-textview-scrollable-on-android
+        TextView helpDescription = findViewById(R.id.helpDescription);
+        helpDescription.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, HelpActivity.class);
     }
-
-
-
-
 }
